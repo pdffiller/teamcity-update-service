9b48fd7b7eba858940dad826b506e423d9807b64
Vladyslav Gaivoronskyi <korax@ukr.net>
2017-05-26 12:55:50 +0300

    Update taskdef.py

--- taskdef.py
+++ taskdef.py
@@ -1,10 +1,29 @@
+Skip to content
+This repository
+Search
+Pull requests
+Issues
+Marketplace
+Gist
+ @vgaivoronskyi
+ Sign out
+ Watch 6
+  Star 0
+ Fork 0 pdffiller/teamcity-update-service
+ Code  Issues 0  Pull requests 0  Projects 0  Wiki  Settings Insights 
+Tree: 79181364cf Find file Copy pathteamcity-update-service/taskdef.py
+7918136  on 28 Mar
+@vgaivoronskyi vgaivoronskyi fix for volumes
+1 contributor
+RawBlameHistory     
+93 lines (73 sloc)  2.31 KB
 import json
 import jsonpickle
 from pprint import pprint
 
 with open('describe_taskdefinition.json') as data_file:
     data = json.load(data_file)
-data = data["taskDefinition"]
+data = data['taskDefinition']
 
 class host:
     def __init__(self):
@@ -21,16 +40,6 @@ class portMappings:
         self.containerPort = 0
         self.hostPort = 0
 
-class logOptions:
-    def __init__(self):
-        self.max_size = "1m"
-        self.max_file = "1"
-
-class logConfiguration:
-    def __init__(self):
-        self.logDriver = "json-file"
-        self.options = []
-
 class env:
     def __init__(self):
         self.name = ""
@@ -41,12 +50,6 @@ class mountPoint:
         self.containerPath = ""
         self.sourceVolume = ""
 
-class ulimits:
-    def __init__(self):
-        self.name = ""
-        self.softLimit = 0
-        self.hardLimit = 0
-
 class containerDefinitions:
     def __init__(self):
         self.environment = []
@@ -56,13 +59,8 @@ class containerDefinitions:
         self.cpu = 0
         self.memory = 0
         self.essential = False
-        self.user = ""
         self.volumesFrom = []
         self.portMappings = []
-        self.logConfiguration = ""
-        self.ulimits = []
-        self.dnsServers = []
-        self.hostname = ""
 
 class deploymentConfiguration:
     def __init__(self):
@@ -72,7 +70,7 @@ class deploymentConfiguration:
 class outData:
     def __init__(self):
         self.family = ""
-        self.networkMode = ""
+        #self.networkMode = ""
         #self.taskDefinition = ""
         self.containerDefinitions = []
         self.volumes = []
@@ -80,10 +78,9 @@ class outData:
 cd = containerDefinitions()
 od = outData()
 od.family = data["family"]
-
+#od.networkMode = data["networkMode"]
 taskDefinition = data["taskDefinitionArn"][data["taskDefinitionArn"].find("/")+1:data["taskDefinitionArn"].rfind(":"):]
-if ("networkMode" in data):
-    od.networkMode = data["networkMode"]
+
 for vol in data["volumes"]:
     v = volume()
     v.host.sourcePath = vol["host"]["sourcePath"]
@@ -95,24 +92,6 @@ cd.image = data["containerDefinitions"][0]["image"]
 cd.cpu = data["containerDefinitions"][0]["cpu"]
 cd.memory = data["containerDefinitions"][0]["memory"]
 cd.essential = data["containerDefinitions"][0]["essential"]
-if ("hostname" in data["containerDefinitions"][0]):
-    cd.hostname = data["containerDefinitions"][0]["hostname"]
-if ("user" in data["containerDefinitions"][0]):
-    cd.user = data["containerDefinitions"][0]["user"]
-cd.logConfiguration = logConfiguration()
-cd.logConfiguration.options = logOptions()
-if ("logConfiguration" in data["containerDefinitions"][0]):
-    cd.logConfiguration.logDriver = data["containerDefinitions"][0]["logConfiguration"]["logDriver"]
-    cd.logConfiguration.options.max_file = data["containerDefinitions"][0]["logConfiguration"]["options"]["max-file"]
-    cd.logConfiguration.options.max_size = data["containerDefinitions"][0]["logConfiguration"]["options"]["max-size"]
-
-if ("dnsServers" in data["containerDefinitions"][0]):
-    cd.dnsServers = data["containerDefinitions"][0]["dnsServers"]
-
-if ("ulimits" in data["containerDefinitions"][0]):
-    cd.ulimits = ulimits()
-    for ul in data["containerDefinitions"][0]["ulimits"]:
-        cd.mountPoints.append(ul)
 
 for mpN in data["containerDefinitions"][0]["mountPoints"]:
     cd.mountPoints.append(mpN)
@@ -125,10 +104,10 @@ for pm in data["containerDefinitions"][0]["portMappings"]:
 
 od.containerDefinitions.append(cd)
 
-pprint(jsonpickle.encode(od, unpicklable=False))
-
 fileName = "describe_taskdefinition_" + taskDefinition + ".json"
 f = open(fileName,"w")
 f.write(jsonpickle.encode(od, unpicklable=False))
 
 
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
