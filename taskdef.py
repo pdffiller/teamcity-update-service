074b1c6b429ddb3e5cfcca5a2c074ec4d0464d9b
Vladyslav Gaivoronskyi <korax@ukr.net>
2017-05-26 16:11:00 +0300

    v 2.0
    change json library

--- taskdef.py
+++ taskdef.py
@@ -1,90 +1,21 @@
-import json
-import jsonpickle
+import jsontree
 from pprint import pprint
 
-with open('describe_taskdefinition.json') as data_file:
-    data = json.load(data_file)
-data = data['taskDefinition']
+with open('taskdef.json', 'r') as myfile:
+    data2=myfile.read().replace('\n', '')
 
-class host:
-    def __init__(self):
-        self.sourcePath = ""
+je = jsontree.JSONTreeDecoder().decode(data2)
+je = je.taskDefinition
 
-class volume:
-    def __init__(self):
-        self.name = ""
-        self.host = host()
+od = jsontree.jsontree()
+od.containerDefinitions = je.containerDefinitions
+od.networkMode = je.networkMode
+od.family = je.family
+od.volumes = je.volumes
 
-class portMappings:
-    def __init__(self):
-        self.protocol = ""
-        self.containerPort = 0
-        self.hostPort = 0
+#pprint(jsontree.JSONTreeEncoder().encode(od))
 
-class env:
-    def __init__(self):
-        self.name = ""
-        self.value = ""
-
-class mountPoint:
-    def __init__(self):
-        self.containerPath = ""
-        self.sourceVolume = ""
-
-class containerDefinitions:
-    def __init__(self):
-        self.environment = []
-        self.name = ""
-        self.mountPoints = []
-        self.image = ""
-        self.cpu = 0
-        self.memory = 0
-        self.essential = False
-        self.volumesFrom = []
-        self.portMappings = []
-
-class deploymentConfiguration:
-    def __init__(self):
-        self.minimumHealthyPercent = 0
-        self.maximumPercent = 100
-
-class outData:
-    def __init__(self):
-        self.family = ""
-        #self.networkMode = ""
-        #self.taskDefinition = ""
-        self.containerDefinitions = []
-        self.volumes = []
-
-cd = containerDefinitions()
-od = outData()
-od.family = data["family"]
-#od.networkMode = data["networkMode"]
-taskDefinition = data["taskDefinitionArn"][data["taskDefinitionArn"].find("/")+1:data["taskDefinitionArn"].rfind(":"):]
-
-for vol in data["volumes"]:
-    v = volume()
-    v.host.sourcePath = vol["host"]["sourcePath"]
-    v.name = vol["name"]
-    od.volumes.append(v)
-
-cd.name = data["containerDefinitions"][0]["name"]
-cd.image = data["containerDefinitions"][0]["image"]
-cd.cpu = data["containerDefinitions"][0]["cpu"]
-cd.memory = data["containerDefinitions"][0]["memory"]
-cd.essential = data["containerDefinitions"][0]["essential"]
-
-for mpN in data["containerDefinitions"][0]["mountPoints"]:
-    cd.mountPoints.append(mpN)
-
-for env0 in data["containerDefinitions"][0]["environment"]:
-    cd.environment.append(env0)
-
-for pm in data["containerDefinitions"][0]["portMappings"]:
-    cd.portMappings.append(pm)
-
-od.containerDefinitions.append(cd)
-
-fileName = "describe_taskdefinition_" + taskDefinition + ".json"
+taskDefinition = je["taskDefinitionArn"][je["taskDefinitionArn"].find("/")+1:je["taskDefinitionArn"].rfind(":"):]
+fileName = "taskdef_" + taskDefinition + ".json"
 f = open(fileName,"w")
-f.write(jsonpickle.encode(od, unpicklable=False))
+f.write(jsontree.JSONTreeEncoder().encode(od))
