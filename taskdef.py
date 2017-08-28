675982157205fea3cb7bb1fb6b9b8e358a984209
Vladyslav Gaivoronskyi <korax@ukr.net>
2017-08-28 18:07:37 +0300

    Update taskdef.py

--- taskdef.py
+++ taskdef.py
@@ -1,5 +1,21 @@
+import sys
 import jsontree
-from pprint import pprint
+
+if len(sys.argv) < 2:
+        with open('img.json', 'r') as data_file:
+                data = data_file.read().replace('\n','')
+
+        data = jsontree.JSONTreeDecoder().decode(data)
+        data = data.imageIds
+        for img in data:
+                if img.imageTag == "latest":
+                        hash = img.imageDigest
+
+        for img in data:
+                if (img.imageDigest == hash) and (img.imageTag!="latest") :
+                        number = img.imageTag
+else:
+        number = sys.argv[1]
 
 with open('describe_taskdefinition.json', 'r') as myfile:
     data2=myfile.read().replace('\n', '')
@@ -8,14 +24,17 @@ je = jsontree.JSONTreeDecoder().decode(data2)
 je = je.taskDefinition
 od = jsontree.jsontree()
 od.containerDefinitions = je.containerDefinitions
-if (je.has_key("networkMode")):
-    od.networkMode = je.networkMode
+if od.containerDefinitions[0].image.rfind(':') > 0 :
+        od.containerDefinitions[0].image = od.containerDefinitions[0].image[0:od.containerDefinitions[0].image$
+else:
+        od.containerDefinitions[0].image += ':' + number
+
 od.family = je.family
 od.volumes = je.volumes
 
-#pprint(jsontree.JSONTreeEncoder().encode(od))
-
-taskDefinition = je["taskDefinitionArn"][je["taskDefinitionArn"].find("/")+1:je["taskDefinitionArn"].rfind(":"):]
+taskDefinition = je["taskDefinitionArn"][je["taskDefinitionArn"].find("/")+1:je["taskDefinitionArn"].rfind(":"$
 fileName = "describe_taskdefinition_" + taskDefinition + ".json"
 f = open(fileName,"w")
 f.write(jsontree.JSONTreeEncoder().encode(od))
+
+#print(jsontree.JSONTreeEncoder().encode(od))
